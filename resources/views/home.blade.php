591c1aa61c6581b2725a081d9b767f6d107ee713
Amir vTarikhi <etarikhi@gmail.com>
2025-03-09 21:01:08 +0330

    Add dashboard view and update routing; rename app name to 'Laravel Dashboard'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.base')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-full lg:w-1/2">
+            <h1 class="text-2xl font-semibold text-gray-800">Home</h1>
+            <p class="mt-2 text-gray-600">Welcome to your Home.</p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,360 @@
+<nav class="global-nav__nav" aria-label="Primary Navigation">
+    <ul class="global-nav__primary-items
+
+        ">
+        <li class="global-nav__primary-item
+              ">
+
+
+            <a class="CkxUBVZenYcRwKJEjEVtZebhlSfMWr  global-nav__primary-link--active
+   global-nav__primary-link" target="_self" href="https://www.linkedin.com/feed/?doFeedRefresh=true&amp;nis=true&amp;"
+                data-test-app-aware-link="" aria-current="page">
+
+
+                <div id="ember1681" class="global-nav__primary-link-notif artdeco-notification-badge ember-view"> <span
+                        class="notification-badge notification-badge--show ">
+                        <span aria-hidden="true" class="notification-badge__no-count"></span>
+                        <span class="a11y-text" data-test-notification-a11y="">new feed updates notifications</span>
+                    </span>
+
+
+                    <div class="ivm-image-view-model    global-nav__icon-ivm
+      ">
+
+                        <div class="ivm-view-attr__img-wrapper
+
+  ">
+                            <!---->
+                            <li-icon aria-hidden="true" type="home-active" active="true" class="ivm-view-attr__icon  "
+                                size="large"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                    data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24"
+                                    height="24" focusable="false">
+                                    <path
+                                        d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z">
+                                    </path>
+                                </svg></li-icon>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+                <span class="t-12 break-words block t-black--light t-normal
+    global-nav__primary-link-text" title="Home">
+                    Home
+                </span>
+            </a>
+
+
+            <!---->
+            <!----><!----><!---->
+        </li>
+        <!---->
+        <li class="global-nav__primary-item
+              ">
+
+
+            <a class="CkxUBVZenYcRwKJEjEVtZebhlSfMWr
+   global-nav__primary-link" target="_self" href="https://www.linkedin.com/mynetwork/?" data-test-app-aware-link="">
+
+
+
+                <div class="ivm-image-view-model    global-nav__icon-ivm
+      ">
+
+                    <div class="ivm-view-attr__img-wrapper
+
+  ">
+                        <!---->
+                        <li-icon aria-hidden="true" type="people" class="ivm-view-attr__icon  " size="large"><svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24"
+                                fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                <path
+                                    d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z">
+                                </path>
+                            </svg></li-icon>
+                    </div>
+
+                </div>
+
+
+
+                <span class="t-12 break-words block t-black--light t-normal
+    global-nav__primary-link-text" title="My Network">
+                    My Network
+                </span>
+            </a>
+
+
+            <!---->
+            <!----><!----><!---->
+        </li>
+        <!---->
+        <li class="global-nav__primary-item
+              ">
+
+
+            <a class="CkxUBVZenYcRwKJEjEVtZebhlSfMWr
+   global-nav__primary-link" target="_self" href="https://www.linkedin.com/jobs/?" data-test-app-aware-link="">
+
+
+
+                <div class="ivm-image-view-model    global-nav__icon-ivm
+      ">
+
+                    <div class="ivm-view-attr__img-wrapper
+
+  ">
+                        <!---->
+                        <li-icon aria-hidden="true" type="job" class="ivm-view-attr__icon  " size="large"><svg
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24"
+                                fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                <path
+                                    d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z">
+                                </path>
+                            </svg></li-icon>
+                    </div>
+
+                </div>
+
+
+
+                <span class="t-12 break-words block t-black--light t-normal
+    global-nav__primary-link-text" title="Jobs">
+                    Jobs
+                </span>
+            </a>
+
+
+            <!---->
+            <!----><!----><!---->
+        </li>
+        <!---->
+        <li class="global-nav__primary-item
+              ">
+
+
+            <a class="CkxUBVZenYcRwKJEjEVtZebhlSfMWr
+   global-nav__primary-link" target="_self" href="https://www.linkedin.com/messaging/?" data-test-app-aware-link="">
+
+
+
+                <div class="ivm-image-view-model    global-nav__icon-ivm
+      ">
+
+                    <div class="ivm-view-attr__img-wrapper
+
+  ">
+                        <!---->
+                        <li-icon aria-hidden="true" type="nav-small-messaging-icon" class="ivm-view-attr__icon  "
+                            size="large"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24"
+                                height="24" focusable="false">
+                                <path
+                                    d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z">
+                                </path>
+                            </svg></li-icon>
+                    </div>
+
+                </div>
+
+
+
+                <span class="t-12 break-words block t-black--light t-normal
+    global-nav__primary-link-text" title="Messaging">
+                    Messaging
+                </span>
+            </a>
+
+
+            <!---->
+            <!----><!----><!---->
+        </li>
+        <!---->
+        <li class="global-nav__primary-item
+              ">
+
+
+            <a class="CkxUBVZenYcRwKJEjEVtZebhlSfMWr
+   global-nav__primary-link" target="_self" href="https://www.linkedin.com/notifications/?"
+                data-test-app-aware-link="">
+
+
+                <div id="ember1683" class="global-nav__primary-link-notif artdeco-notification-badge ember-view">
+                    <span class="notification-badge notification-badge--show ">
+                        <span aria-hidden="true" class="notification-badge__count ">1</span>
+                        <span class="a11y-text" data-test-notification-a11y="">1 new notification</span>
+                    </span>
+
+
+                    <div class="ivm-image-view-model    global-nav__icon-ivm
+      ">
+
+                        <div class="ivm-view-attr__img-wrapper
+
+  ">
+                            <!---->
+                            <li-icon aria-hidden="true" type="bell-fill" class="ivm-view-attr__icon  " size="large"><svg
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24"
+                                    fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                    <path
+                                        d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z">
+                                    </path>
+                                </svg></li-icon>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+                <span class="t-12 break-words block t-black--light t-normal
+    global-nav__primary-link-text" title="Notifications">
+                    Notifications
+                </span>
+            </a>
+
+
+            <!---->
+            <!----><!----><!---->
+        </li>
+        <!---->
+        <li class="global-nav__primary-item
+              ">
+            <!---->
+            <div id="ember15"
+                class="global-nav__me artdeco-dropdown artdeco-dropdown--placement-bottom artdeco-dropdown--justification-left ember-view">
+                <button aria-expanded="false" id="ember16"
+                    class="global-nav__primary-link global-nav__primary-link-me-menu-trigger artdeco-dropdown__trigger artdeco-dropdown__trigger--placement-bottom ember-view"
+                    type="button" tabindex="0">
+                    <img width="24"
+                        src="https://media.licdn.com/dms/image/v2/D4D35AQGBO3k5u7MWTA/profile-framedphoto-shrink_100_100/B4DZVDZodrGkAs-/0/1740592548054?e=1742144400&amp;v=beta&amp;t=FWFFdF3ptVy7aaLlMxF0ide66DBI_r6z3Ms6fcLH8Ls"
+                        height="24" alt="Amir vTarikhi" id="ember17" class="global-nav__me-photo evi-image ember-view">
+
+                    <span class="t-12 global-nav__primary-link-text">
+                        Me
+                        <svg role="none" aria-hidden="true" class="global-nav__icon global-nav__icon--small"
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"
+                            data-supported-dps="16x16" data-test-icon="caret-small">
+                            <!---->
+                            <use href="#caret-small" width="16" height="16"></use>
+                        </svg>
+
+                    </span>
+
+                    <!----></button>
+
+                <div tabindex="-1" aria-hidden="true" id="ember18"
+                    class="global-nav__me-content artdeco-dropdown__content artdeco-dropdown--is-dropdown-element artdeco-dropdown__content--justification-right artdeco-dropdown__content--placement-bottom ember-view">
+                    <!---->
+                </div>
+            </div>
+
+            <!---->
+            <!----><!---->
+        </li>
+        <!---->
+        <li class="global-nav__primary-item
+              ">
+            <!----><!---->
+            <div id="ember19"
+                class="global-nav__primary-item--divider pl3
+  global-nav__app-launcher-menu artdeco-dropdown artdeco-dropdown--placement-bottom artdeco-dropdown--justification-left ember-view">
+                <button aria-expanded="false" id="ember20"
+                    class="global-nav__primary-link global-nav__primary-link-app-launcher-menu-trigger artdeco-dropdown__trigger artdeco-dropdown__trigger--placement-bottom ember-view"
+                    type="button" tabindex="0">
+
+                    <div class="ivm-image-view-model    global-nav__icon-ivm">
+
+                        <div class="ivm-view-attr__img-wrapper
+
+  ">
+                            <!---->
+                            <li-icon aria-hidden="true" type="grid" class="ivm-view-attr__icon  " size="large"><svg
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24"
+                                    fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+                                    <path
+                                        d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z">
+                                    </path>
+                                </svg></li-icon>
+                        </div>
+
+                    </div>
+
+
+                    <span class="t-12 global-nav__primary-link-text" title="For Business">
+                        For Business
+                        <svg role="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 16 16" data-supported-dps="16x16" data-test-icon="caret-small">
+                            <!---->
+                            <use href="#caret-small" width="16" height="16"></use>
+                        </svg>
+
+                    </span>
+
+                    <!---->
+                </button>
+
+                <div tabindex="-1" aria-hidden="true" id="ember21"
+                    class="global-nav__app-launcher-menu-content artdeco-dropdown__content artdeco-dropdown--is-dropdown-element artdeco-dropdown__content--justification-right artdeco-dropdown__content--placement-bottom ember-view">
+                    <!---->
+                </div>
+            </div>
+
+            <!---->
+        </li>
+        <!---->
+        <li class="global-nav__primary-item
+              ">
+            <!----><!----><!---->
+
+            <a class="CkxUBVZenYcRwKJEjEVtZebhlSfMWr
+   global-nav__primary-link" target="_blank" rel="noopener noreferrer"
+                href="https://www.linkedin.com/learning/?trk=nav_neptune_learning&amp;" data-test-app-aware-link="">
+
+
+
+                <div class="ivm-image-view-model    global-nav__icon-ivm
+      ">
+
+                    <div class="ivm-view-attr__img-wrapper
+
+  ">
+                        <!---->
+                        <li-icon aria-hidden="true" type="nav-small-learning-icon" active="true"
+                            class="ivm-view-attr__icon  " size="large"><svg xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24"
+                                height="24" focusable="false">
+                                <path
+                                    d="M22 5v14H2V5h20m1-2H1a1 1 0 00-1 1v16a1 1 0 001 1h22a1 1 0 001-1V4a1 1 0 00-1-1z">
+                                </path>
+                                <path d="M2 5v14h10V5H2zm8 12H4v-2h6v2zm0-4H4v-2h6v2zm0-4H4V7h6v2z"
+                                    style="isolation:isolate" opacity=".25"></path>
+                                <path style="isolation:isolate" opacity=".6"
+                                    d="M14 7h6v2h-6zM14 11h6v2h-6zM14 15h6v2h-6z"></path>
+                                <path
+                                    d="M10 7.53v8.93a.28.28 0 00.44.23l6.43-4.44a.33.33 0 00.06-.46l-.06-.06-6.43-4.43a.28.28 0 00-.44.23z">
+                                </path>
+                            </svg></li-icon>
+                    </div>
+
+                </div>
+
+
+
+                <span class="t-12 break-words block t-black--light t-normal
+    global-nav__primary-link-text" title="Learning">
+                    Learning
+                </span>
+            </a>
+
+
+            <!---->
+        </li>
+        <!---->
+        <!---->
+    </ul>
+</nav>
